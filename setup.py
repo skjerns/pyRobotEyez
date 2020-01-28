fe6735d241ed250ba87ac617e61a3fd840a19ac8
skjerns <14980558+skjerns@users.noreply.github.com>
2020-01-28 13:31:34 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyRobotEyez',
+      version='1.0',
+      description='A simple webcam capture script',
+      url='http://github.com/skjerns/pyRobotEyez',
+      author='skjerns',
+      author_email='nomail',
+      license='GNU 2.0',
+      packages=['pyroboteyez'],
+      zip_safe=False)
